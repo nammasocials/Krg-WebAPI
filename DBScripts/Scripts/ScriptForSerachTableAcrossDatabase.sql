5656a456f9c2d61e224dfb2279286167c44aad83
S Anand <Anand@fusionintek.com>
2025-11-10 09:13:02 +0530

    Search across table script

---
+++ DBScripts/Scripts/ScriptForSerachTableAcrossDatabase.sql
@@ -0,0 +1,22 @@
+CREATE TABLE #Results (
+    DatabaseName SYSNAME,
+    SchemaName SYSNAME,
+    TableName SYSNAME
+);
+
+EXEC sp_msforeachdb '
+DECLARE @TableName NVARCHAR(256) = ''InvCustomers''
+IF ''?'' NOT IN (''master'',''tempdb'',''model'',''msdb'')
+BEGIN
+    INSERT INTO #Results
+    SELECT ''?'' AS DatabaseName,
+           s.name AS SchemaName,
+           t.name AS TableName
+    FROM [?].sys.tables t
+    INNER JOIN [?].sys.schemas s ON t.schema_id = s.schema_id
+    WHERE t.name = @TableName
+END
+';
+
+SELECT * FROM #Results;
+DROP TABLE #Results;
